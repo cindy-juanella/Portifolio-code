ed64e11f7186a889812556973288aeace6f0b436
cindy-juanella <72721492+cindy-juanella@users.noreply.github.com>
2023-04-28 15:20:45 -0300

    Update SET.js

--- SET.js
+++ SET.js
@@ -1,14 +1,14 @@
-//SET : evita duplicada de palavras, e tags por exemplo
+//SET : evita duplicata de palavras, e tags por exemplo
 // 
 
 var novoSet = new Set();
 
 novoSet.add('backend');
 novoSet.add('frontend');
-novoSet.add('mopbile');
+novoSet.add('mobile');
 novoSet.add('mobile');
 novoSet.add('mobile');
 
 
 var resultado = novoSet;
-console.log(resultado);
+console.log(resultado); // vai apaecer somente um 'mobile'
